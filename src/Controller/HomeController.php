a9131b7cdfdff3a7d9f8310792962edff908a7f9
broyer damien <broyerdamien@gmail.com>
2023-02-01 23:30:53 +0100

    function login and front create documentation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
@@ -12,7 +13,25 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'website' => 'Wiki-wild',
+        ]);
+    }
+
+    #[Route('/login', name: 'app_homes')]
+
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('home/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+            'website' => 'Wiki-wild',
+
         ]);
     }
 }
--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,14 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout(): void
     {
-        return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
-        ]);
     }
 }
