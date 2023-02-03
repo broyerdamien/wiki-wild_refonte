34191d039a7f67ac159b49dcd7763faa2258d595
broyer damien <broyerdamien@gmail.com>
2023-02-03 10:06:55 +0100

    dark mode

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,10 @@
  */
 console.log('Hello Webpack Encore !')
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import '../styles/app.scss';
 
 // start the Stimulus application
-import './bootstrap';
+import '../bootstrap';
 
 require('bootstrap');
+require('./darkMode.js');
